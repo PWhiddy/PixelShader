a3d84bf193925812947f50b6ccdbb1c861e2a8b9
Peter Whidden <whidp@amazon.com>
2018-08-18 16:25:25 -0700

    noise

---
+++ noise.h
@@ -0,0 +1,42 @@
+#include "cutil_math.cuh"
+
+//#define MOD3 make_float3(.1031,.11369,.13787)
+//#define MOD3 float3(443.8975,397.2973, 491.1871)
+constexpr float3 MOD3 = make_float3(.1031,.11369,.13787);
+
+float hash31(float3 p3)
+{
+	p3  = fract(p3 * MOD3);
+    p3 += dot(p3, p3.yzx + 19.19);
+    return -1.0 + 2.0 * fract((p3.x + p3.y) * p3.z);
+}
+
+float3 hash33(float3 p3)
+{
+	p3 = fract(p3 * MOD3);
+    p3 += dot(p3, p3.yxz+19.19);
+    return -1.0 + 2.0 * fract(make_float3((p3.x + p3.y)*p3.z, (p3.x+p3.z)*p3.y, (p3.y+p3.z)*p3.x));
+}
+
+float simplex_noise(float3 p)
+{
+    const float K1 = 0.333333333;
+    const float K2 = 0.166666667;
+    
+    float3 i = floor(p + (p.x + p.y + p.z) * K1);
+    float3 d0 = p - (i - (i.x + i.y + i.z) * K2);
+    
+    // thx nikita: https://www.shadertoy.com/view/XsX3zB
+    float3 e = step(make_float3(0.0), d0 - d0.yzx);
+	float3 i1 = e * (1.0 - e.zxy);
+	float3 i2 = 1.0 - e.zxy * (1.0 - e);
+    
+    float3 d1 = d0 - (i1 - 1.0 * K2);
+    float3 d2 = d0 - (i2 - 2.0 * K2);
+    float3 d3 = d0 - (1.0 - 3.0 * K2);
+    
+    float4 h = max(0.6 - make_float4(dot(d0, d0), dot(d1, d1), dot(d2, d2), dot(d3, d3)), 0.0);
+    float4 n = h * h * h * h * make_float4(dot(d0, hash33(i)), dot(d1, hash33(i + i1)), dot(d2, hash33(i + i2)), dot(d3, hash33(i + 1.0)));
+    
+    return dot(make_float4(31.316), n);
+}
