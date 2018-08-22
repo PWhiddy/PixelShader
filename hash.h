3707c7940c7cf1ecdd0746c1ee119389f61c2d14
Peter Whidden <whidp@amazon.com>
2018-08-22 00:30:38 -0700

    better hash

--- hash.h
+++ hash.h
@@ -1,5 +1,5 @@
 #include "cutil_math.h"
-
+/*
 // From https://nullprogram.com/blog/2018/07/31/
 __device__ __forceinline__ unsigned hash(unsigned a) 
 {
@@ -22,8 +22,19 @@ __device__ __forceinline__ unsigned hash3(unsigned a, unsigned b, unsigned c) {
 __device__ __forceinline__ unsigned hash4(unsigned a, unsigned b, unsigned c, unsigned d) {
     return hash2(a,b) ^ hash2(c,d);
 }
+*/
 
-__device__ __forceinline__ float hash2intfloat(unsigned a, unsigned b) {
+// https://www.shadertoy.com/view/4dlcR4
+uint hash2(uint x, uint y)
+{
+  x *= 0x3504f335u;   // 15 | 101 | 41*79*274627
+  y *= 0x8fc1ecd5u;   // 18 | 101 | 5*482370193
+  x ^= y;    // combine
+  x *= 741103597u;     // 18 | 101 | 5*482370193
+  return x;
+}
+
+__device__ __forceinline__ float hash2intfloat(uint a, uint b) {
     return __uint2float_rd( hash2(a,b) );
 }
 
