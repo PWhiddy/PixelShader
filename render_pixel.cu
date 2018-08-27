21ad22cdbbc880d8857dc3727bb2c43a76b5d076
Peter Whidden <whidp@amazon.com>
2018-08-26 20:49:05 -0700

    update

--- render_pixel.cu
+++ render_pixel.cu
@@ -81,19 +81,19 @@ __global__ void render_pixel (
     float uvy = -float(y)/float(y_dim)+0.5;
     uvx *= float(x_dim)/float(y_dim);     
 
-    float3 light_dir = normalize(make_float3(0.1, 1.0, -0.5));
+    //float3 light_dir = normalize(make_float3(0.1, 1.0, -0.5));
 
     // Set up ray originating from camera
     float3 ray_pos = make_float3(0.0, 0.0, -1.5);
     //float2 pos_rot = rotate(make_float2(ray_pos.x, ray_pos.z), 0.0);
     //ray_pos.x = pos_rot.x;
     //ray_pos.z = pos_rot.y;
-    float3 ray_dir = glm::normalize(make_float3(uvx,uvy,0.5));
+    //float3 ray_dir = normalize(make_float3(uvx,uvy,0.5));
     //float2 dir_rot = rotate(make_float2(ray_dir.x, ray_dir.z), 0.0);
-    ray_dir.x = dir_rot.x;
-    ray_dir.z = dir_rot.y;
+    //ray_dir.x = dir_rot.x;
+    //ray_dir.z = dir_rot.y;
 
-    float3 color = make_float3(0.95);
+    float3 color = make_float3(0.95,0.92,0.96);
     const int max_bounces = 6;
     /*
     //for (int bounce = 0; bounce < max_bounces; bounce++)
