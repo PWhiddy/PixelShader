3c912b17dc190041bc87c7a2790070547a40a0b0
Peter Whidden <whidp@amazon.com>
2018-08-26 23:00:21 -0700

    update

--- render_pixel.cu
+++ render_pixel.cu
@@ -8,24 +8,23 @@
 //#include "noise.h"
 //#include "cuda_noise.h"
 
-/*
-__device__ float2 rotate(float2 p, float a)
+
+__device__ glm::vec2 rotate(glm::vec2 p, float a)
 {
-    return make_float2(p.x*cos(a) - p.y*sin(a),
-                       p.y*cos(a) + p.x*sin(a));
-}
-*/
-/*
-__device__ float sdSphere(float3 p, float r) {
-    return length(p)-r;
+    return glm::vec2(p.x*cos(a) - p.y*sin(a),
+                     p.y*cos(a) + p.x*sin(a));
 }
 
-__device__ float sdBox( float3 p, float3 b )
-{
-      float3 d = fabs(p) - b;
-      return fminf(fmaxf(d.x,fmaxf(d.y,d.z)),0.0f) + length(fmaxf(d,make_float3(0.0f)));
+__device__ float sdSphere(glm::vec3 p, float r) {
+    return glm::length(p)-r;
 }
-*/
+
+__device__ float sdBox( glm::vec3 p, glm::vec3 b )
+{
+    glm::vec3 d = glm::vec3abs(p) - b;
+      return glm::min(glm::max(d.x,glm::max(d.y,d.z)),0.0f) + glm::length(glm::max(d,glm::vec3(0.0f)));
+}
+
 /*
 __device__ float fractalNoise(float3 p) {
     p += 300.0f;
@@ -40,24 +39,24 @@ __device__ float fractalNoise(float3 p) {
     return result;
 }
 */
-/*
-__device__ float map(float3 p, float t) {
+
+__device__ float map(glm::vec3 p, float t) {
     float d;
-    d =  sdSphere(p, 0.8)+0.025f*fractal4(make_float4(4.0f*p.x+50.0f,4.0f*p.y+50.0f,4.0f*p.x+50.0f,0.008f*t));
+    d =  sdSphere(p, 0.8);//+0.025f*fractal_noise(glm::vec3(4.0f*p.x,4.0f*p.y,0.008f*t));
     //d = fminf(-sdBox(p, make_float3(2.0,2.0,2.0)), d);
     return d;
 }
 
-__device__ float3 calcNormal( float3 pos, float t )
+__device__ glm::vec3 calcNormal( glm::vec3 pos, float t )
 {
-    float2 e = make_float2(1.0,-1.0)*0.5773*0.0005;
-    return normalize( make_float3(e.x,e.y,e.y)*map( pos + make_float3(e.x,e.y,e.y), t ) + 
-					  make_float3(e.y,e.y,e.x)*map( pos + make_float3(e.y,e.y,e.x), t ) + 
-					  make_float3(e.y,e.x,e.y)*map( pos + make_float3(e.y,e.x,e.y), t ) + 
-					  make_float3(e.x,e.x,e.x)*map( pos + make_float3(e.x,e.x,e.x), t ) );
+    glm::vec2 e = glm::vec2(1.0,-1.0)*0.5773f*0.0005f;
+    return glm::normalize( glm::vec3(e.x,e.y,e.y)*map( pos + glm::vec3(e.x,e.y,e.y), t ) + 
+					  glm::vec3(e.y,e.y,e.x)*map( pos + glm::vec3(e.y,e.y,e.x), t ) + 
+					  glm::vec3(e.y,e.x,e.y)*map( pos + glm::vec3(e.y,e.x,e.y), t ) + 
+					  glm::vec3(e.x,e.x,e.x)*map( pos + glm::vec3(e.x,e.x,e.x), t ) );
 }
 
-__device__ float3 intersect(float3 ray_pos, float3 ray_dir, float t)
+__device__ float3 intersect(glm::vec3 ray_pos, glm::vec3 ray_dir, float t)
 {
     for (int i=0; i<1024; i++) {
         float dist = map(ray_pos, t);
@@ -66,7 +65,6 @@ __device__ float3 intersect(float3 ray_pos, float3 ray_dir, float t)
     }
     return ray_pos;
 }
-*/
 
 __device__ __forceinline__ glm::vec3 hash33(glm::vec3 p3)
 {
@@ -134,18 +132,18 @@ __global__ void render_pixel (
     //float3 light_dir = normalize(make_float3(0.1, 1.0, -0.5));
 
     // Set up ray originating from camera
-    float3 ray_pos = make_float3(0.0, 0.0, -1.5);
-    //float2 pos_rot = rotate(make_float2(ray_pos.x, ray_pos.z), 0.0);
-    //ray_pos.x = pos_rot.x;
-    //ray_pos.z = pos_rot.y;
-    //float3 ray_dir = normalize(make_float3(uvx,uvy,0.5));
-    //float2 dir_rot = rotate(make_float2(ray_dir.x, ray_dir.z), 0.0);
-    //ray_dir.x = dir_rot.x;
-    //ray_dir.z = dir_rot.y;
+    glm::vec3 ray_pos = glm::vec3(0.0, 0.0, -1.5);
+    glm::vec2 pos_rot = rotate(glm::vec2(ray_pos.x, ray_pos.z), 0.0);
+    ray_pos.x = pos_rot.x;
+    ray_pos.z = pos_rot.y;
+    glm::vec3 ray_dir = glm::normalize(make_float3(uvx,uvy,0.5));
+    glm::vec2 dir_rot = rotate(glm::vec2(ray_dir.x, ray_dir.z), 0.0);
+    ray_dir.x = dir_rot.x;
+    ray_dir.z = dir_rot.y;
 
-    float3 color = make_float3(0.95,0.92,0.96);
+    glm::vec3 color = glm::vec3(0.95,0.92,0.96);
     const int max_bounces = 6;
-    /*
+    
     //for (int bounce = 0; bounce < max_bounces; bounce++)
     //{
         ray_pos = intersect(ray_pos, ray_dir, time);
@@ -153,11 +151,11 @@ __global__ void render_pixel (
     //}
     
     
-    float3 background = make_float3(0.87);
-    float3 normal = calcNormal(ray_pos, time);
-    float value = dot(normal,light_dir);
-    color = make_float3(value, value, value);
-    if (length(ray_pos) > 10.0) color = background;
+    glm::vec3 background = glm::vec3(0.87);
+    glm::vec3 normal = calcNormal(ray_pos, time);
+    float value = glm::dot(normal,light_dir);
+    color = glm::vec3(value, value, value);
+    if (glm::length(ray_pos) > 10.0) color = background;
     //color = make_float3(rng::simplexNoise(make_float3(uvx*25.0,uvy*25.0,0.0)+100.0, 1.0, 123));
 
     /*
@@ -205,13 +203,13 @@ __global__ void render_pixel (
     //color.z = __uint2float_rd(rand.z) * conv_range;
     
     //float val = fractal4( make_float4( float(x)*0.002f, float(y)*0.002f, 6.0f, float(time_step)*0.07f ) );
-    float val = 0.5;
-    glm::vec3 position = glm::vec3(float(x)*0.01f, float(y)*0.01f, 1.0f);
-    val = fractal_noise(position);
+    //float val = 0.5;
+    //glm::vec3 position = glm::vec3(float(x)*0.01f, float(y)*0.01f, 1.0f);
+    //val = fractal_noise(position);
     //val = 0.5f*simplex_noise(position) + 0.5f;
 
     const int pixel = 3*((y-y_offset)*x_dim+x);
-    image[pixel+0] = (uint8_t)(fmin(255.0f*val, 255.0f));
-    image[pixel+1] = (uint8_t)(fmin(255.0f*val, 255.0f));
-    image[pixel+2] = (uint8_t)(fmin(255.0f*val, 255.0f));
+    image[pixel+0] = (uint8_t)(fmin(255.0f*color.x, 255.0f));
+    image[pixel+1] = (uint8_t)(fmin(255.0f*color.y, 255.0f));
+    image[pixel+2] = (uint8_t)(fmin(255.0f*color.z, 255.0f));
 }
